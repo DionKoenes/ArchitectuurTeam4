9ae0bc506abdedd0460a447d0f815d719737851e
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2021-06-18 11:44:10 +0200

    Emiel moving materials

---
+++ Assets/Scenes/Emiel/Scripts/MovingTexture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTexture : MonoBehaviour
+{
+    [SerializeField]
+    private float scrollSpeed;
+    [SerializeField]
+    private float offsetX;
+    private Renderer render;
+    void Start()
+    {
+        scrollSpeed = 1;
+        offsetX = -2;
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offsetX += scrollSpeed * Time.deltaTime;
+        render.material.mainTextureOffset = new Vector3(0f, offsetX, 1f);
+    }
+}
---
+++ Assets/Scenes/Emiel/Scripts/Teleporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform endPos;
+    public Player player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = endPos.position;
+    }
+}
