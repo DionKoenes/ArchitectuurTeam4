3120f2099d3a9ef152300b0407f69220b694c60b
BlackVoid194 <84567212+BlackVoid194@users.noreply.github.com>
2021-06-25 09:03:53 +0200

    apotheek toegevoegd

---
+++ Assets/Scenes/Ilse/SliderErlenmyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderErlenmyer : MonoBehaviour
+{
+    float scrollSpeed;
+    float offsetX;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollSpeed = 0.1f;
+        offsetX = 0f;
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offsetX -= scrollSpeed * Time.deltaTime;
+        rend.material.mainTextureOffset = new Vector3(offsetX, offsetX, offsetX);
+    }
+}
